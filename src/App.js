d986017965b21e39a7e296520a1ab65b13e71c5e
reedt <dharit.tan@gmail.com>
2022-04-06 22:42:25 -0400

    first commit - picture + list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavCard from "./components/NavCard.js";
 
 function App() {
   return (
     <div className="App">
+      <NavCard />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
